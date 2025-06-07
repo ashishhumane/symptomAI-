45af8118ac2d92ba2a48783520ca6035df8aff69
Ashish Humane <ashishhumane2003@gmail.com>
2025-06-07 12:10:21 +0530

    completed mvp design

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '../client')));
+
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/signup', (req, res) => {
+  res.render('signup')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+app.get('/profile', (req, res) => {
+  res.render('profile')
+})
+
+app.get('/about', (req, res) => {
+  res.render('aboutus')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contactus')
+})
+
+app.get('/logSymptom', (req, res) => {
+  res.render('symptomlog')
+})
+
+
+
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+})
